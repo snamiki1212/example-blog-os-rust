f5f5bb7a2c9eeb16a07c010ea2d3e5cf8a268961
snamiki1212 <snamiki1212@gmail.com>
2022-09-25 14:59:18 -0700

    feat: add test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-#![no_main]
 #![no_std]
+#![no_main]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -18,7 +15,29 @@ pub extern "C" fn _start() -> ! {
     // // for check panic msg
     // panic!("Some panic message");
 
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion(){
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}

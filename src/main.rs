a66b29849b1c1824506796ba18ffea8365d71eff
snamiki1212 <snamiki1212@gmail.com>
2022-09-25 03:02:22 -0700

    feat: add vgb

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -17,12 +18,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // let vga_buffer = 0xb8000 as *mut u8;
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+
+    vga_buffer::print_something();
     loop {}
 }
